279b3f44d59bbf27631fa7ad37230e0fcd32daa3
Jefferson Ondze Mangha <34817366+JeffersonOndzeMangha@users.noreply.github.com>
2023-12-07 03:33:40 -0600

    added account module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AccountController } from './account/account.controller';
+import { AccountService } from './account/account.service';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    AccountModule,
+  ],
+  controllers: [
+    AppController,
+    AccountController
+  ],
+  providers: [
+    AppService,
+    AccountService
+  ],
 })
 export class AppModule {}
