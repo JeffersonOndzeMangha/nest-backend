09bbe7a7570761956fee2f716160633ad44be8e4
Jefferson Ondze Mangha <34817366+JeffersonOndzeMangha@users.noreply.github.com>
2023-12-07 03:42:10 -0600

    update and clean up, generated all the necessary modules

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
+import { AccountController } from './account.controller';
 
 @Module({
-  imports: [],
-  controllers: [AccountController],
   providers: [AccountService],
+  controllers: [AccountController]
 })
 export class AccountModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,32 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AccountController } from './account/account.controller';
 import { AccountService } from './account/account.service';
+import { TransactionModule } from './transaction/transaction.module';
 import { AccountModule } from './account/account.module';
+import { PersonModule } from './person/person.module';
+import { TransactionController } from './transaction/transaction.controller';
+import { PersonController } from './person/person.controller';
+import { TransactionService } from './transaction/transaction.service';
+import { PersonService } from './person/person.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     AccountModule,
+    TransactionModule,
+    PersonModule,
   ],
   controllers: [
     AppController,
-    AccountController
+    AccountController,
+    TransactionController,
+    PersonController
   ],
   providers: [
     AppService,
-    AccountService
+    AccountService,
+    TransactionService,
+    PersonService
   ],
 })
 export class AppModule {}
