19722e534259df3af393d5c2586ead69b698b7d8
Jefferson Ondze Mangha <34817366+JeffersonOndzeMangha@users.noreply.github.com>
2023-12-07 22:15:25 -0600

    update tests with new methods

--- src/account/account.service.spec.ts
+++ src/account/account.service.spec.ts
@@ -4,6 +4,8 @@ import { DB, DBProvider } from '../database';
 import { Account, AccountType } from '../database/types';
 import { v4 as uuid4 } from 'uuid';
 
+export const toFixedNumber = (num: number) => Number(num.toFixed(2));
+
 describe('AccountService', () => {
   let accountService: AccountService;
   let database: DB;
@@ -31,8 +33,8 @@ describe('AccountService', () => {
       const accountData:Account = {
         id: uuid4(),
         owner: uuid4(),
-        balance: 700,
-        dailyWithdrawalLimit: 30,
+        balance: toFixedNumber(Math.random() * 10000),
+        dailyWithdrawalLimit: toFixedNumber(Math.random() * 100),
         accountType: AccountType.CHECKING,
         transactions: [],
         createdDate: new Date().toLocaleDateString(),
@@ -67,7 +69,7 @@ describe('AccountService', () => {
     it('should update an account', async () => {
       const id = Object.keys(database.data)[0];
       const accountData = {
-        balance: 1000,
+        balance: toFixedNumber(database.data[id].balance + (Math.random() * 100)),
       }
       const expectedResult = {
         ...database.data[id],
@@ -81,10 +83,84 @@ describe('AccountService', () => {
 
   describe('deleteAccount', () => {
     it('should delete an account', async () => {
-      const id = Object.keys(database.data)[0];
-
+      const id = Object.keys(database.data)[Object.keys(database.data).length - 2];
       const result = await accountService.deleteAccount(id);
       expect(result).toEqual(id);
     });
+  });
+
+  describe('deposit', () => {
+    it('should deposit money into an account', async () => {
+      const id = Object.keys(database.data)[0];
+      const accountData = {
+        amount: toFixedNumber(Math.random() * 100),
+      }
+      const expectedResult = {
+        ...database.data[id],
+        balance: database.data[id].balance + accountData.amount,
+      };
+
+      const result = await accountService.deposit(id, accountData);
+      expect(result).toEqual(expectedResult);
+    });
+  });
+
+  describe('withdraw', () => {
+    it('should withdraw money from an account', async () => {
+      const id = Object.keys(database.data)[0];
+      const accountData = {
+        amount: toFixedNumber(Math.random() * 100),
+      }
+      const expectedResult = {
+        ...database.data[id],
+        balance: database.data[id].balance - accountData.amount,
+      };
+
+      const result = await accountService.withdraw(id, accountData);
+      expect(result).toEqual(expectedResult);
+    });
+  });
+
+  describe('transfer', () => {
+    it('should transfer money from one account to another', async () => {
+      const id = Object.keys(database.data)[0];
+      const secondId = Object.keys(database.data)[1];
+
+      const accountData = {
+        amount: toFixedNumber(Math.random() * 100),
+        destinationAccount: secondId,
+      }
+
+      const expectedResult = {
+        [id]: {
+          ...database.data[id],
+          balance: database.data[id].balance - accountData.amount,
+        },
+        [secondId]: {
+          ...database.data[secondId],
+          balance: database.data[secondId].balance + accountData.amount,
+        },
+      }
+
+      const result = await accountService.transfer(id, accountData);
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should throw an error when the destination account does not exist', async () => {
+      const id = Object.keys(database.data)[0];
+      const secondId = uuid4();
+
+      const accountData = {
+        amount: toFixedNumber(Math.random() * 100),
+        destinationAccount: secondId,
+      }
+
+      try {
+        await accountService.transfer(id, accountData);
+      } catch (error) {
+        expect(error.message).toEqual('Destination account not found');
+      }
+    });
+
   });
 });
